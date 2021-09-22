3d9cc2e8ea04c3f3493d0e0c0781bb2abe977995
MichMatch <michael@graphicdesire.se>
2021-09-22 16:14:31 +0200

    tabs for recipe details

--- lib/data/recipes_api.dart
+++ lib/data/recipes_api.dart
@@ -6,6 +6,12 @@ class RecipeApi {
     return http.get("http://localhost:3002/api/get");
   }
   static Future getSingleRecipe(int id) {
-    return http.get("http://localhost:3002/api/getFromId/" + id.toString());
+    return http.get("http://localhost:3002/api/getFromId/$id");
+  }
+  static Future getIngredientsFromId(int id) {
+    return http.get("http://localhost:3002/api/getIngredientsFromId/$id");
+  }
+  static Future getStepsFromId(int id) {
+    return http.get("http://localhost:3002/api/getStepsFromId/$id");
   }
 }
--- lib/screens/recipe_details.dart
+++ lib/screens/recipe_details.dart
@@ -21,46 +21,123 @@ class _RecipeDetailsState extends State<RecipeDetails> {
   }
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Details'),
-      ),
-      body: Container(
-        padding: const EdgeInsets.all(35),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Image.asset("assets/images/${widget.recipe.id}.jpg"),
-            Padding(
-              padding: EdgeInsets.all(10),
+    return DefaultTabController(
+        initialIndex: 0,
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            titleTextStyle: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            title: Text(widget.recipe.title),
+            bottom: const TabBar(
+              tabs: <Widget>[
+                Tab(
+                  icon: Text('Recipe Details'),
+                ),
+                Tab(
+                  icon: Text('Ingredients'),
+                ),
+                Tab(
+                  icon: Text('Steps'),
+                ),
+              ],
             ),
-            Text(
-              widget.recipe.title,
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, fontFamily: "UniSans"),
-            ),
-            Padding(
-              padding: EdgeInsets.all(5),
-            ),
-            Text(
-              widget.recipe.description,
-              style: TextStyle(fontSize: 17, fontFamily: "Raleway", fontWeight: FontWeight.w400),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 0),
-              child: NeumorphicContainer(
-                height: 40,
-                width: 40,
-                borderRadius: 40,
-                spread: 2,
-                primaryColor: Color(0xfff0f0f0),
-                //flat neumorphism design
-                curvature: Curvature.concave,
-                child: Icon(Icons.favorite_outline, color: Colors.red, size: 23)
+          ),
+        body: TabBarView (
+          children: <Widget> [
+            Container(
+              padding: const EdgeInsets.all(35),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Image.asset("assets/images/${widget.recipe.id}.jpg"),
+                  Padding(
+                    padding: EdgeInsets.all(10),
+                  ),
+                  Text(
+                    widget.recipe.title,
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, fontFamily: "UniSans"),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(5),
+                  ),
+                  Text(
+                    widget.recipe.description,
+                    style: TextStyle(fontSize: 17, fontFamily: "Raleway", fontWeight: FontWeight.w400),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 0),
+                    child: NeumorphicContainer(
+                      height: 40,
+                      width: 40,
+                      borderRadius: 40,
+                      spread: 2,
+                      primaryColor: Color(0xfff0f0f0),
+                      //flat neumorphism design
+                      curvature: Curvature.concave,
+                      child: Icon(Icons.favorite_outline, color: Colors.red, size: 23)
+                  ),
+                  ),
+                  ],
+                  ),
+                ),
+            Container(
+              padding: const EdgeInsets.all(35),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    "ingredients you need",
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, fontFamily: "UniSans"),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(5),
+                  ),
+                  Text(
+                    widget.recipe.description,
+                    style: TextStyle(fontSize: 17, fontFamily: "Raleway", fontWeight: FontWeight.w400),
+                  ),
+                ],
               ),
             ),
-          ],
-        ),
-      ),
-    );
-  }
+            Container(
+              padding: const EdgeInsets.all(35),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Image.asset("assets/images/${widget.recipe.id}.jpg"),
+                  Padding(
+                    padding: EdgeInsets.all(10),
+                  ),
+                  Text(
+                    widget.recipe.title,
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, fontFamily: "UniSans"),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(5),
+                  ),
+                  Text(
+                    widget.recipe.description,
+                    style: TextStyle(fontSize: 17, fontFamily: "Raleway", fontWeight: FontWeight.w400),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 0),
+                    child: NeumorphicContainer(
+                        height: 40,
+                        width: 40,
+                        borderRadius: 40,
+                        spread: 2,
+                        primaryColor: Color(0xfff0f0f0),
+                        //flat neumorphism design
+                        curvature: Curvature.concave,
+                        child: Icon(Icons.favorite_outline, color: Colors.red, size: 23)
+                    ),
+                  ),
+                ],
+              ),
+            ),
+                ],
+                ),
+                ),
+              );
+            }
 }
